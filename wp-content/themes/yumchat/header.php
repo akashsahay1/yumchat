af437306c97979156e2109b384be406873eb7874
Akash <akash.sahay1@gmail.com>
2023-11-07 17:33:19 +0530

    Modified header file

--- wp-content/themes/yumchat/header.php
+++ wp-content/themes/yumchat/header.php
@@ -44,20 +44,20 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <script src="<?php bloginfo('template_directory'); ?>/js/modernizr-1.6.min.js"></script>
 
-<?php
-    /* We add some JavaScript to pages with the comment form
-     * to support sites with threaded comments (when in use).
-     */
-    if ( is_singular() && get_option( 'thread_comments' ) )
-        wp_enqueue_script( 'comment-reply' );
+    <?php
+        /* We add some JavaScript to pages with the comment form
+        * to support sites with threaded comments (when in use).
+        */
+        if ( is_singular() && get_option( 'thread_comments' ) )
+            wp_enqueue_script( 'comment-reply' );
 
-    /* Always have wp_head() just before the closing </head>
-     * tag of your theme, or you will break many plugins, which
-     * generally use this hook to add elements to <head> such
-     * as styles, scripts, and meta tags.
-     */
-    wp_head();
-?>
+        /* Always have wp_head() just before the closing </head>
+        * tag of your theme, or you will break many plugins, which
+        * generally use this hook to add elements to <head> such
+        * as styles, scripts, and meta tags.
+        */
+        wp_head();
+    ?>
 </head>
 
 <body <?php body_class(); ?>>
